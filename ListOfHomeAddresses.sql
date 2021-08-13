52c56054a992a2bd423801b18b6a1dc20dcf0cef
Sarah St. Martin <56164730+sstmartin1@users.noreply.github.com>
2021-08-13 13:19:59 -0400

    Create ListOfHomeAddresses.sql

---
+++ ListOfHomeAddresses.sql
@@ -0,0 +1,13 @@
+select p.Barcode,pr.PatronFullName,addr.StreetOne,addr.StreetTwo,pos.City,pos.State,pos.PostalCode
+from Polaris.PatronRegistration pr with (nolock)
+inner join Polaris.Patrons p with (nolock)
+on (pr.PatronID = p.PatronID)
+left join Polaris.PatronAddresses pa with (nolock)
+on (pr.PatronID = pa.PatronID)
+left join Polaris.Addresses addr with (nolock)
+on (pa.AddressID = addr.AddressID)
+left join Polaris.PostalCodes pos with (nolock)
+on (addr.PostalCodeID = pos.PostalCodeID)
+where p.PatronCodeID in (7,17,18,20,25,27,28,32,34,42,47)
+AND pa.AddressLabelID = 1
+order by StreetOne
